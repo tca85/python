600942adcbeecc213070876df8c75eb423f92616
tca85 <thiago.alves.85@gmail.com>
2016-10-11 19:50:18 -0400

    Alura - aula 6

--- Alura/app.py
+++ Alura/app.py
@@ -9,6 +9,7 @@ Curso Python I: Programando com a linguagem
 
 @author: tca85
 '''
+from mercurial.templater import if_
 
 #-----------------------------------------------------------------------------------------
 def cadastrar(nomes):
@@ -22,14 +23,35 @@ def listar(nomes):
     
     for nome in nomes:
         print nome
+
+#-----------------------------------------------------------------------------------------
+def remover(nomes):
+    print 'Qual nome você gostaria de remover?'
+    nome = raw_input()
     
+    if len(nome) != 0:
+        nomes.remove(nome)
+
+#-----------------------------------------------------------------------------------------
+def alterar(nomes):
+    print 'Qual nome você gostaria de alterar?'
+    nome_inserido = raw_input()
+    
+    for indice, nome in enumerate(nomes):
+        if nome == nome_inserido:
+            print 'Informe o novo nome'
+            nomes[indice] = raw_input()
+        else:
+            print 'Nome não encontrado'
+    
+        
 #-----------------------------------------------------------------------------------------
 def menu():
     nomes = []
     escolha = ''
     
     while (escolha != '0'):
-        print 'Digite: 1 para cadastrar, 0 para terminar'
+        print '\n1 - Cadastrar \n2 - Listar \n3 - Remover \n4 - Alterar \n0 - Sair'
         escolha = raw_input()
         
         if(escolha == '1'):
@@ -37,6 +59,12 @@ def menu():
         
         if(escolha == '2'):
             listar(nomes)
+        
+        if(escolha == '3'):
+            remover(nomes)
+            
+        if(escolha == '4'):
+            alterar(nomes)
             
 #-----------------------------------------------------------------------------------------
 menu()
