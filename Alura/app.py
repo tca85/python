e804d7f9b5b416fbb53b277c2da0792fc1c914ff
tca85 <thiago.alves.85@gmail.com>
2016-10-11 20:12:10 -0400

    Alura - aula 6

--- Alura/app.py
+++ Alura/app.py
@@ -9,7 +9,6 @@ Curso Python I: Programando com a linguagem
 
 @author: tca85
 '''
-from mercurial.templater import if_
 
 #-----------------------------------------------------------------------------------------
 def cadastrar(nomes):
@@ -44,14 +43,32 @@ def alterar(nomes):
         else:
             print 'Nome não encontrado'
     
+#-----------------------------------------------------------------------------------------
+def alterar_nomes_solucao2(nomes):
+    print 'Qual nome vc gostaria de alterar?'
+    nome_a_alterar = raw_input()
         
+    if(nome_a_alterar in nomes):
+        posicao = nomes.index(nome_a_alterar)
+        print 'Digite novo nome:'
+        nome_novo = raw_input()
+        nomes[posicao] = nome_novo
+#-----------------------------------------------------------------------------------------
+def procurar(nomes):
+    print 'Digite nome a procurar:'
+    nome_a_procurar = raw_input()
+
+    if(nome_a_procurar in nomes):
+        print 'O nome', nomes[nomes.index(nome_a_procurar)],  'está na posição', nomes.index(nome_a_procurar)
+
+
 #-----------------------------------------------------------------------------------------
 def menu():
     nomes = []
     escolha = ''
     
     while (escolha != '0'):
-        print '\n1 - Cadastrar \n2 - Listar \n3 - Remover \n4 - Alterar \n0 - Sair'
+        print '\n1 - Cadastrar \n2 - Listar \n3 - Remover \n4 - Alterar \n5 - Procurar \n0 - Sair'
         escolha = raw_input()
         
         if(escolha == '1'):
@@ -65,6 +82,9 @@ def menu():
             
         if(escolha == '4'):
             alterar(nomes)
+            
+        if(escolha == '5'):
+            procurar(nomes)
             
 #-----------------------------------------------------------------------------------------
 menu()
