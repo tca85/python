5078396aee5746c382b1d15b1ad2b2ba67c7d964
tca85 <thiago.alves.85@gmail.com>
2016-10-15 11:52:16 -0400

    aula 8 OOP basico

--- Alura/TestarPerfil.py
+++ Alura/TestarPerfil.py
@@ -8,6 +8,7 @@ Created on Oct 15, 2016
 
 from models.Data import Data
 from models.Perfil import Perfil
+from models.Pessoa import Pessoa
 
 
 perfil1 = Perfil('Thiago Cordeiro Alves', '+1 647 455 xxxx', 'SAP Labs Toronto')
@@ -26,3 +27,6 @@ print perfil.__class__
 
 d = Data(21, 11, 2007)
 d.imprime()
+
+pessoa = Pessoa("Ronaldo", 105, 1.78)
+pessoa.imprime_imc() 
