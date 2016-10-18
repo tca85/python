e16ebb336bb9b18fa676be628cb224e52468325d
tca85 <thiago.alves.85@gmail.com>
2016-10-17 21:16:03 -0400

    Aula 1 - Strategy - modularizando com função

--- design-patterns/calculador_de_impostos.py
+++ design-patterns/calculador_de_impostos.py
@@ -9,6 +9,8 @@ Design Patterns Python I: Boas práticas de programação
 @author: tca85
 '''
 
+from impostos import calcula_ISS, calcula_ICMS
+
 #=============================================================================================
 class Calculador_de_impostos(object):
     
@@ -19,14 +21,15 @@ class Calculador_de_impostos(object):
     def realiza_calculo(self, orcamento, imposto):
         
         # Essa não é a forma ideal porque se houver mais um imposto teremos que modificar o código
+        # podemos melhorar um pouco modularizando o código fazendo a chamada de uma função
+        # impostos.calcula_ICMS
         if 'ICMS' == imposto:
-            icms_calculado = orcamento.valor * 0.1
+            icms_calculado = calcula_ICMS(orcamento.valor)
             print icms_calculado
             
         elif 'ISS' == imposto:
-            iss_calculado = orcamento.valor * 0.06
+            iss_calculado = calcula_ISS(orcamento.valor)
             print iss_calculado
-        
         
     #-----------------------------------------------------------------------------------------
 #=============================================================================================    
