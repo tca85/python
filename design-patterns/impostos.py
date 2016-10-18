2c77d2a94a64763f64c424c9330480766bdc87a7
tca85 <thiago.alves.85@gmail.com>
2016-10-17 21:16:14 -0400

    Aula 1 - Strategy - modularizando com função

---
+++ design-patterns/impostos.py
@@ -0,0 +1,13 @@
+# -*- coding: UTF-8 -*-
+
+'''
+Created on Oct 17, 2016
+
+@author: tca85
+'''
+
+def calcula_ICMS(orcamento):
+    return orcamento.valor * 0.1
+
+def calcula_ISS(orcamento):
+    return orcamento.valor * 0.06
