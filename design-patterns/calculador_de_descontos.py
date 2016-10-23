2149c17c6696b79b96c29ac33674dd218dc75bdb
tca85 <thiago.alves.85@gmail.com>
2016-10-23 17:18:26 -0400

    Aula 5 - State e re-organização do código

--- design-patterns/calculador_de_descontos.py
+++ design-patterns/calculador_de_descontos.py
@@ -28,21 +28,3 @@ class Calculador_de_descontos(object):
     #-----------------------------------------------------------------------------------------
 
 #=============================================================================================
-
-'''
- A chamada da função main está sendo feita aqui para evitar criar uma classe para testar
-'''
-    
-if __name__ == '__main__':
-    from orcamento import Orcamento, Item
-    
-    orcamento = Orcamento()
-    
-    orcamento.adiciona_item(Item('Item A', 100.0))
-    orcamento.adiciona_item(Item('Item B', 50.0))
-    orcamento.adiciona_item(Item('Item C', 400.0))
-    
-    calculador_de_descontos = Calculador_de_descontos()
-    desconto = calculador_de_descontos.calcula(orcamento)
-    print 'Desconto calculado : %s' % (desconto)
-
